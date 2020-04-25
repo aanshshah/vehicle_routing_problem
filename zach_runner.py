126b6e08d8fc72a9a0dae564739711586d80f47a
Zachary Horvitz <zacharyhorvitz@users.noreply.github.com>
2020-04-25 18:05:18 -0400

    Added iterative strategy, greedy (deter + stochastic) + simmulated anneal

---
+++ zach_runner.py
@@ -0,0 +1,109 @@
+import argparse
+from random_solution import RandomStrategy
+from greedy_hill_climbing import GreedyHillClimbing
+from zach_test import Iterative_Solution
+import os
+
+
+def read_instance(filename):
+		with open(filename, 'r') as f:
+			lines = f.readlines()
+		num_customers, num_vehicles, vehicle_capacity = [int(x) for x in lines[0].split()]
+		_, depo_x, depo_y = [float(x) for x in lines[1].split()]
+		customer_info = [[0,0,depo_x, depo_y]]
+		idx = 1
+		for line in lines[2:]:
+			line = line.split()
+			if line:
+				customer = [idx] + [float(x) for x in line]
+				idx += 1
+				if customer:
+					customer_info.append(customer) # idx, customer_demand, customer_x, customer_y
+		return [depo_x, depo_y], [num_customers, num_vehicles, vehicle_capacity], customer_info
+
+
+def format_path(paths, save_solution=False):
+	formatted_path = ''
+	if save_solution:
+		for path in paths:
+			formatted_path += ' '.join(str(x) for x in path)
+	else:
+		for idx, path in enumerate(paths):
+			formatted_path += 'truck {0}: '.format(str(idx + 1))
+			formatted_path += ' '.join(str(x) for x in path)
+			formatted_path += '\n'
+	return formatted_path
+
+def get_test_files():
+	test_files = []
+	for file in os.listdir('input'):
+		test_files.append(os.path.join('input', file))
+	return test_files
+
+def run_all(strategy):
+	for file in get_test_files():
+		run_single(file,strategy)
+
+def run_single(file, strategy, print_out=True):
+	instance = read_instance(file)
+	strategy = get_strategy(strategy, instance)
+	paths, distance = strategy.run()
+	if paths and distance and print_out:
+		print('Strategy: '+strategy.name)
+		print(format_path(paths))
+		print("distance: {0}".format(str(distance)))
+	else:
+		print("NO SOLUTION FOUND FOR {0}".format(file))
+
+
+#find best seed across all files
+def test_seeds(strategy_name):
+	num_seeds = 10000
+	best_seed = -1.0
+	total_distance = 0
+	min_distance = float("inf")
+	for seed in range(num_seeds):
+		for file in get_test_files():
+			instance = read_instance(file)
+			strategy = get_strategy(strategy_name, instance, seed)
+			paths, distance = strategy.run()
+			total_distance += distance
+		if total_distance < min_distance:
+			best_seed = seed
+			min_distance = distance
+	print(best_seed)
+	return best_seed, min_distance
+
+def get_strategy(strategy, instance, seed=0):
+	if strategy == 'random':
+		return RandomStrategy(instance, seed=seed)
+	if strategy == 'random_greedy':
+		return RandomStrategy(instance, greedy=True, seed=seed)
+	if strategy == 'greedy_hill':
+		return GreedyHillClimbing(instance)
+	else:
+		return Iterative_Solution(instance)
+
+
+
+
+
+def main(filename, single, strategy, test):
+	if test and strategy:
+		#best seed found for random was 0
+		#random_greedy took really long and best seed was 
+		test_seeds(strategy)
+	elif single and filename:
+		run_single(filename, strategy)
+	else:
+		run_all(strategy)
+
+if __name__ == '__main__':
+	# run_all("Zach")
+	parser = argparse.ArgumentParser()
+	parser.add_argument('filename', nargs='?')
+	parser.add_argument('-s', '--single', action='store_true')
+	parser.add_argument('-a', '--strategy', default='random') #, choices=['random', 'random_greedy', 'greedy_hill'])
+	parser.add_argument('-t', '--test', action='store_true')
+	args = parser.parse_args()
+	main(**vars(args))
---
+++ zach_test.py
@@ -0,0 +1,160 @@
+import random 
+from utils import calculate_distance
+from strategy import Strategy
+import numpy as np
+
+class Iterative_Solution(Strategy):
+	def __init__(self, instance, greedy=False, seed=0, epsilon=1):
+		super().__init__(instance)
+		random.seed(seed)
+		self.greedy = greedy
+		self.name = "iterative_sol"
+		self.vehicle_map = {i : (self.vehicle_capacity, 0, [0]) for i in range(self.num_vehicles)}
+		self.epsilon = epsilon
+
+	def get_initial_solution(self):
+		def random_search():
+			if self.greedy:
+				self.customer_info.sort(key=lambda x: x[1])
+			else:
+				random.shuffle(self.customer_info)
+			visited = set()
+			total_distance_traveled = 0
+			all_truck_paths = []
+			for i in range(self.num_vehicles): 
+				capacity = self.vehicle_capacity 
+				truck_path = [0]
+				for customer_idx, customer_demand, _, _ in self.customer_info:
+					if customer_idx in visited:
+						continue		
+					if capacity - customer_demand < 0:
+						continue
+					visited.add(customer_idx)
+					capacity -= customer_demand
+					truck_path.append(customer_idx)
+					if capacity == 0: break 
+				truck_path.append(0) #goes back to depot
+				all_truck_paths.append(truck_path)
+			return all_truck_paths
+
+		while True:
+			self.attempts += 1
+			visited = set()
+			all_truck_paths = random_search()
+			for path in all_truck_paths:
+				for customer_idx in path:
+					visited.add(customer_idx)
+			if len(visited) == self.num_customers:
+				return all_truck_paths
+
+	def approach(self):
+		all_truck_paths = self.get_initial_solution()
+		all_truck_paths, total_distance_traveled =  self.iterate_on_solution(all_truck_paths,iterations=1000,selector=greedy)
+		# all_truck_paths, total_distance_traveled = self.iterate_on_solution(all_truck_paths,iterations=10000,selector=simmulated_annealing)
+
+		return all_truck_paths, total_distance_traveled 
+
+	def calculate_total_distance(self,solution):
+		total_distance_traveled = 0
+		for truck_locations in solution:
+			for start,end in zip(truck_locations[:-1],truck_locations[1:]):
+				_,_,start_x,start_y = self.customer_info[start]
+				_,_,end_x,end_y = self.customer_info[end]
+
+				total_distance_traveled += calculate_distance(start_x, start_y, end_x, end_y)
+		return total_distance_traveled
+					
+
+	def apply_swap(self,solution,swap):
+		starting_index = swap[0]
+		ending_index = swap[1]
+
+		ele_to_move = solution[starting_index[0]][starting_index[1]]
+		del solution[starting_index[0]][starting_index[1]]
+		solution[ending_index[0]].insert(ending_index[1],ele_to_move)
+
+		return solution,ending_index[0]
+
+	def undo_swap(self,solution,swap):
+		starting_index = swap[0]
+		ending_index = swap[1]
+		ele_to_move = solution[ending_index[0]][ending_index[1]]
+		del solution[ending_index[0]][ending_index[1]]
+		solution[starting_index[0]].insert(starting_index[1],ele_to_move)		
+		return solution
+
+	def get_all_neighbors(self,truck_paths):
+		swaps = []
+
+		for truck_number,locations in enumerate(truck_paths):
+			# print(truck_number)
+			if len(locations) < 2: continue
+			else:
+				for i,l in enumerate(locations[1:-1]):
+					index = i + 1
+					new_solution = truck_paths #.copy()
+					for new_truck_number,truck_locations in enumerate(new_solution):
+						for j in range(len(truck_locations)-2):
+							swaps.append(((truck_number,index),(new_truck_number,j+1)))
+		return swaps
+
+	def evaluate_neighbors(self,truck_paths,sample_neighbors=False,sample_size=-1):
+		solution = truck_paths
+		swaps = self.get_all_neighbors(truck_paths)
+
+		if sample_neighbors:
+			random.shuffle(swaps)
+			swaps = swaps[:sample_size]
+
+		evaluated_swaps = []
+		for s in swaps:
+			solution, new_truck = self.apply_swap(solution,s)
+			total_truck_demand = sum([self.customer_info[c][1] for c in solution[new_truck][1:-1]])
+			if self.vehicle_capacity >= total_truck_demand:
+				score = self.calculate_total_distance(solution)
+			else: score = np.Inf
+			evaluated_swaps.append((s,score))
+			self.undo_swap(solution,s)
+		assert truck_paths == solution
+		return evaluated_swaps
+
+	def iterate_on_solution(self,truck_paths,selector,iterations=1000,stop_if_no_progress=False):
+			#For every vehicle, for every customer, for every location
+
+			solution = truck_paths
+			objective_value = self.calculate_total_distance(solution)
+			print("initial:",objective_value)
+			for step in range(iterations):
+				previous_value = objective_value
+				swaps = self.evaluate_neighbors(solution,sample_neighbors=True,sample_size=100)
+				chosen = selector(swaps,previous_value,step,iterations)
+				if chosen is not None:
+					solution,_ = self.apply_swap(solution,chosen)
+				objective_value = self.calculate_total_distance(solution)
+				if step % 100 == 0: print("step: {}, cost: {}".format(step,objective_value))
+
+				if stop_if_no_progress:
+					if previous_value == objective_value: break;
+
+			return solution,objective_value
+
+def greedy(swaps,previous_value=None,step=None,max_step=None):
+		greedy_best = min(swaps,key=lambda x:x[-1])
+		chosen,value = greedy_best
+		return chosen
+
+def simmulated_annealing(swaps,previous_value,step,max_step):
+	sample,value = swaps[0]
+
+	if value < previous_value:
+		return sample
+	else:
+		T = 100*np.exp(-0.001*(step+1/max_step)) #Temp function. Neeed to tune
+		if step % 1000 == 0: print(T)
+		accept_P = np.exp(-(value-previous_value)/T) #(Kirkpatrick et al.,)
+		# print(accept_P)
+		if accept_P >= np.random.rand():
+			return sample
+		else: return None
+
+
