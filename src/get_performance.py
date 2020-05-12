dcebb386d082fefd266208d57a6eb83b2172318f
Aansh Shah <aansh.shah@gmail.com>
2020-05-12 11:04:10 -0400

    added more details to performance script

--- src/get_performance.py
+++ src/get_performance.py
@@ -11,17 +11,33 @@ def determine_leaderboard_performance(filepath):
             performance[instance_name] = (screenname, distance)
     return performance
 
-def get_my_best_performance(filepath, my_screenname):
-    leaderboard_performance = determine_leaderboard_performance(filepath)
+def get_my_best_performance(leaderboard_performance, my_screenname):
     counter = 0
     for instance_name, (screenname, _) in leaderboard_performance.items():
         if screenname == my_screenname: counter += 1
     return counter
 
+def get_all_other_best_performances(leaderboard_performance)
+    # best = {screenname : 0 for instance_name, (screenname, _) in leaderboard_performance.items()}
+    best = {}
+    for instance_name, (screenname, _) in leaderboard_performance.items()::
+        best[screenname] = best.get(screenname, 0) + 1
+    return best
+
+def display_all_top_performers(best_performer_counts):
+    sorted_counts = {k: v for k, v in sorted(best_performer_counts.items(), key=lambda item: item[1])}
+    for screenname, count in sorted_counts.items():
+        print("{0} has lowest distance in {1} instances".format(str(screenname), str(count)))
+
 def main():
     my_screenname = "036ebde0"
     filepath = "/course/cs2951o/www/p5aggregate.csv"
-    print(get_my_best_performance(filepath, my_screenname))
+    performance = determine_leaderboard_performance(filepath)
+    counter = get_my_best_performance(performance, my_screenname)
+    print("We have lowest distance in {0} instances".format(str(counter)))
+    best_performers = get_all_other_best_performances(performance)
+    display_all_top_performers(best_performers)
+
 
 if __name__ == '__main__':
     main()
